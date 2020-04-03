683745b971fc93c1edaecc6bc55a542479e3f2cb
ccarlos <2238513100@qq.com>
2020-04-03 15:51:45 +0800

    RabbitMQ与Spring Cloud Stream整合  生产端项目初始化

---
+++ src/main/java/com/ccarlos/rabbitmqspringcloud/RabbitmqSpringcloudstreamProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ccarlos.rabbitmqspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringcloudstreamProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringcloudstreamProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccarlos/rabbitmqspringcloud/RabbitmqSpringcloudstreamProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.rabbitmqspringcloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RabbitmqSpringcloudstreamProducerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
